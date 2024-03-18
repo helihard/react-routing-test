ce9fe4687dfdca9db4f905c5bd01522d7024c72e
Heli Hård Källoff <vierge.hard@gmail.com>
2024-03-19 00:08:39 +0100

    added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
-import "./App.css"
+import { Link } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Startpage from "./pages/Startpage.jsx"
+import Form from "./pages/Form.jsx"
+import Functionality from "./pages/Functionality"
+import Content from "./pages/Content.jsx"
+
+//import "./App.css"
 
 function App() {
   return (
     <>
-      <h1>Hello World!</h1>
+      <BrowserRouter>
+        <nav>
+          <Link to="/">startsida</Link>&nbsp;
+          <Link to="/form">form</Link>&nbsp;
+          <Link to="/functionality">funktion</Link>&nbsp;
+          <Link to="/content">innehåll</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Startpage />} />
+          <Route path="form" element={<Form />} />
+          <Route path="functionality" element={<Functionality />} />
+          <Route path="content" element={<Content />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
